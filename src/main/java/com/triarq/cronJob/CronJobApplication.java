b559bb0833c3388d293c3a72de263ad89b491d90
manishkanawade <manishkanawade007@gmail.com>
2024-09-07 14:35:38 +0530

    added code for cron job

---
+++ src/main/java/com/triarq/cronJob/CronJobApplication.java
@@ -0,0 +1,21 @@
+package com.triarq.cronJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class CronJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronJobApplication.class, args);
+	}
+	
+	@Scheduled(fixedRate = 5000)
+	public void hello() {
+		System.out.println("hello cron job is executed ");
+	}
+
+}
